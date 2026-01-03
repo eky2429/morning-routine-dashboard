7a45fefb331a718b426189514aa5f93064035d11
Ethan Ky <eky2429@gmail.com>
2026-01-02 21:07:56 -0600

    Finalized features for morning routine app

--- zen_quotes.js
+++ zen_quotes.js
@@ -1,12 +1,13 @@
-const api_url ="https://zenquotes.io/api/quotes";
+const api_url = "https://api.allorigins.win/get?url=https://zenquotes.io/api/quotes";
 
 async function getapi(url){
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
+    var quotes = JSON.parse(data.contents);
+    console.log(quotes);
 
     //Adds quote to h3 element
-    document.getElementById("quote").innerHTML = '"' + data[0]['q'] + '" - ' + data[0]['a'];
+    document.getElementById("quote").innerHTML = '"' + quotes[0]['q'] + '" - ' + quotes[0]['a'];
 }
 
 getapi(api_url);
